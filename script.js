01556896bdede43a82234438307d0802abbbf026
Your Name <you@example.com>
2023-08-21 19:29:22 +0100

    update:layout

---
+++ script.js
@@ -0,0 +1,26 @@
+// function add(id) {
+//     let el = document.getElementById(id);
+//     el.value++;
+//     total();
+//   }
+  
+//   function remove(id) {
+//     let ell = document.getElementById(id);
+//     if (ell.value >= 1) {
+//       ell.value--;
+//     }
+//     total();
+//   }
+  
+//   function total() {
+//     let all = document.querySelectorAll("input");
+//     sum = 0;
+   
+//     // sum += parseInt((all[0].value)*100) + parseInt((all[1].value)* 2) + parseInt((all[2].value)* 3);
+  
+//     let tot = document.querySelector("p");
+//     tot.innerHTML = sum + "dt";
+//   }
+
+const buttons=document.querySelectorAll('button')
+console.log(buttons)
