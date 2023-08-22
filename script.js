e01c00f8c2a014a49a9cbdb797b6080a520cadcc
Your Name <you@example.com>
2023-08-22 19:38:06 +0100

    up-date-version2

--- script.js
+++ script.js
@@ -1,26 +1,103 @@
-// function add(id) {
-//     let el = document.getElementById(id);
-//     el.value++;
-//     total();
-//   }
-  
-//   function remove(id) {
-//     let ell = document.getElementById(id);
-//     if (ell.value >= 1) {
-//       ell.value--;
-//     }
-//     total();
-//   }
-  
-//   function total() {
-//     let all = document.querySelectorAll("input");
-//     sum = 0;
-   
-//     // sum += parseInt((all[0].value)*100) + parseInt((all[1].value)* 2) + parseInt((all[2].value)* 3);
-  
-//     let tot = document.querySelector("p");
-//     tot.innerHTML = sum + "dt";
-//   }
+document.addEventListener("DOMContentLoaded", () => {
+  const itemCart = [];
+  const popUp = document.getElementById("cart");
+  const cartItems = document.getElementById("cart-content");
+  const openCart = document.getElementsByClassName("look-inside")[0];
+  openCart.addEventListener("click", () => {
+    cartItems.innerHTML = "";
+    popUp.style.display = "block";
+  });
+  const hideCart = document.getElementById("close-button");
+  hideCart.addEventListener("click", () => {
+    popUp.style.display = "none";
+  });
+  const buttons = document.getElementsByClassName("add-me");
+  console.log(buttons)
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const nameProduct = button.getAttribute("name-prod");
+      const priceProduct = button.getAttribute("price-prod");
+      const img = button.getAttribute("img-prod");
+      const existingItem = itemCart.find(
+        (item) => item.nameProduct === nameProduct
+      );
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        //add object to the array
+        itemCart.push({ nameProduct, priceProduct, img, quantity: 1 });
+      }
+      displayCart();
+    });
+  });
 
-const buttons=document.querySelectorAll('button')
-console.log(buttons)
+  function displayCart() {
+   
+    cartItems.innerHTML = "";
+    let totalPrice = 0; // Initialize the total price
+    
+    itemCart.forEach((item) => {
+      const cartContent = `
+      <div class="style-cart">
+        <img src="${item.img}" />
+        <h6>${item.nameProduct}</h6>
+        <h6>${item.priceProduct} Dt</h6>
+        <button class="add-button" data-name="${item.nameProduct}">+</button>
+        <h6>Quantity: ${item.quantity}</h6>
+        <button class="remove-button" data-name="${item.nameProduct}">-</button>
+        <button class="remove" data-name="${item.nameProduct}">OUT of Habit</button>
+      </div>
+    `;
+      cartItems.innerHTML += cartContent;
+      // Calculate the price for this item and add it to the total price
+      totalPrice += item.priceProduct * item.quantity;
+    });
+
+    // Display the total price
+    const totalContent = `
+    <div class="total">
+      <h6>Total Price: ${totalPrice.toFixed(2)} Dt</h6>
+    </div>
+  `;
+    cartItems.innerHTML += totalContent;
+
+    // Add event listeners to the add and remove buttons
+    const addButtons = document.querySelectorAll(".add-button");
+    addButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const itemName = button.getAttribute("data-name");
+        const item = itemCart.find((item) => item.nameProduct === itemName);
+        if (item) {
+          item.quantity++;
+          displayCart();
+        }
+      });
+    });
+
+    const removeButtons = document.querySelectorAll(".remove-button");
+    removeButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const itemName = button.getAttribute("data-name");
+        const item = itemCart.find((item) => item.nameProduct === itemName);
+        if (item && item.quantity > 1) {
+          item.quantity--;
+          displayCart();
+        }
+      });
+    });
+
+    const removeProductButtons = document.querySelectorAll(".remove");
+    removeProductButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const itemName = button.getAttribute("data-name");
+        const index = itemCart.findIndex(
+          (item) => item.nameProduct === itemName
+        );
+        if (index !== -1) {
+          itemCart.splice(index, 1);
+          displayCart();
+        }
+      });
+    });
+  }
+});
